ee6847d17cba9a24f3f6ceacccafc98a5c9b789b
ludchieng <lud.chieng@hotmail.fr>
2021-11-25 14:48:38 +0100

    Add sass

--- craco.config.js
+++ craco.config.js
@@ -2,6 +2,8 @@ module.exports = {
     style: {
       postcss: {
         plugins: [
+          require('postcss-import'),
+          require('tailwindcss/nesting'),
           require('tailwindcss'),
           require('autoprefixer'),
         ],
